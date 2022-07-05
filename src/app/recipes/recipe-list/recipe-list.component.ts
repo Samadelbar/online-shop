c40d35e40631c32fc9029505461b97e17fc7de34
Samaneh Delbari <sama.delbari400@gmail.com>
2022-07-05 23:56:49 +0430

    add recipe html

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+ recipes: Recipe[] = [
+  new Recipe ('دستور پخت ماکارونی','یک توضیح تست برای پخت ماکارونی ','https://digifood.ir/wp-content/uploads/2020/05/spaghetti2-digifood.jpg'),
+  new Recipe ('دستور پخت ماکارونی','یک توضیح تست برای پخت ماکارونی قرار می دهیم','https://www.digikala.com/mag/wp-content/uploads/2020/07/macaroni-recipe-1.jpg'),
+  new Recipe ('دستور پخت ماکارونی','یک توضیح تست برای پخت ماکارونی قرار می دهیم','https://www.digikala.com/mag/wp-content/uploads/2020/07/macaroni-recipe-1.jpg')
+ ]
   constructor() { }
 
   ngOnInit(): void {
